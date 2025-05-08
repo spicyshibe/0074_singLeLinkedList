3cd0fe68ff07b46413c19d3bc3d48deeaae4dac9
spicyshibe <rafierasyw@gmail.com>
2025-05-08 19:06:32 +0700

    mengisi bool delnode

--- singlelinklist.cpp
+++ singlelinklist.cpp
@@ -85,6 +85,18 @@ class LinkedList
     }
 
     bool delNode(int nim)
-   
+    {
+        Node *current, *previous;
+        if (Search(nim, &previous, &current))
+        return false;
+
+        if (current == START)
+            START = START->next;
+            else
+            previous->next = current->next;
+            delete current;
+            return true;
+
+    }
 
 };
