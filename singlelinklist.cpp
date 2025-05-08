833471aa4643d1f169f3a09b84f9e413a137910b
spicyshibe <rafierasyw@gmail.com>
2025-05-08 19:05:12 +0700

    mengisi bool search

--- singlelinklist.cpp
+++ singlelinklist.cpp
@@ -70,6 +70,20 @@ class LinkedList
     }
 
     bool Search(int nim, Node **previous, Node **current)
- 
+    {
+        *previous = START;
+        *current = START;
+
+        while ((*current != NULL) && (nim != (*current)->noMhs))
+        {
+            *previous = *current;
+            *current = (*current)->next;
+
+        }
+        return (*current != NULL);
+
+    }
+
+   
 
 };
