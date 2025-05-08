d0f1ac621e3acf4bc399438e0a63980b021ea6c9
spicyshibe <rafierasyw@gmail.com>
2025-05-08 18:34:08 +0700

    membuat class node dan linked list

--- singlelinklist.cpp
+++ singlelinklist.cpp
@@ -3,5 +3,22 @@ using namespace std;
 
 class Node
 {
+    public:
+        int noMhs;
+        Node *next;
 
+};
+
+
+class LinkedList
+{
+    node *START;
+
+    public:
+    LinkedList();
+    {
+        START = NULL;
+    }
+
+    
 }
