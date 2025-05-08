30154cd090d4678be9ae4c508ca05176dc7d6b29
spicyshibe <rafierasyw@gmail.com>
2025-05-08 18:52:23 +0700

    membuat variable dan display

--- singlelinklist.cpp
+++ singlelinklist.cpp
@@ -21,6 +21,14 @@ class LinkedList
     }
 
     void addNode()
-    
+    {
+        int nim;
+        cout << "\nMasukkan Nomor Mahasiswa: ";
+        cin >> nim;
+
+        Node *nodeBaru = new Node;
+        nodeBaru-> noMhs = nim;
+
+       
 
 };
